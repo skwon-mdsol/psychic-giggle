cae5bb9dde3020eb35972c95444e715e54ce8a7e
Sean Kwon <skwon@mdsol.com>
2019-02-11 10:10:32 -0500

    why did i add formik again?

--- client/main.js
+++ client/main.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Formik, Form, Field } from 'formik';
+
 class App extends React.Component {
+  handleSubmit (e) {
+    console.log(e);
+  }
   render () {
-    return <div>Hullo World</div>;
+    return <div>
+      <Formik
+        initialValues={{ category: '', start: null, end: null, description: '' }}
+        onSubmit={this.handleSubmit}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <Form>
+            <Field type="text" name="description" />
+          </Form>
+        )}
+      </Formik>
+    </div>;
   }
 }
 
